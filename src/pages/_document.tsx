7ef0e46517cf95bfb3dea58d6ef0af973f2bba12
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2024-06-17 23:07:03 +0530

    added: Added new font Clash Display

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {Html, Head, Main, NextScript} from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <link href="https://api.fontshare.com/v2/css?f[]=clash-display@200,300,400,500,600,700,1&display=swap"
+                  rel="stylesheet"/>
+            <Head/>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    );
 }
